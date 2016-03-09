931711e7b31a5163accbf2940d7d7b56e4861df8
Leo Santos <leo@arquiweb.com.br>
2016-03-09 18:59:06 -0300

    Implementação Remove Note
    Implementação da funcionalidade remove note.

--- locallib.php
+++ locallib.php
@@ -663,12 +663,12 @@ function icontent_get_pagenotes($pageid, $cmid, $tab){
 			$notecomment = html_writer::div($pagenote->comment, 'notecomment', array('data-pagenoteid'=>$pagenote->id, 'data-cmid'=>$pagenote->cmid, 'data-sesskey' => sesskey()));
 			// Note footer
 			$noteedit = html_writer::link(null, "<i class='fa fa-pencil'></i>".get_string('edit', 'icontent'), array('class'=>'editnote'));
-			$noteremove = html_writer::link('#', "<i class='fa fa-times'></i>".get_string('remove', 'icontent'));
+			$noteremove = html_writer::link("removenote.php?id=".$page->cmid."&pnid=".$pagenote->id, "<i class='fa fa-times'></i>".get_string('remove', 'icontent'), array('class'=>'removenote'));
 			$notelike = icontent_make_likeunlike($page, $pagenote);
 			$notereply = html_writer::link(null, "<i class='fa fa-reply-all'></i>".get_string('reply', 'icontent'), array('class'=>'replynote'));
  			$notedate = html_writer::tag('span', userdate($pagenote->timecreated), array('class'=>'notedate pull-right'));
 			$notefooter = html_writer::div($noteedit. $noteremove. $notereply. $notelike. $notedate, 'notefooter');
-			// verify path levels
+			// Verify path levels
 			$pathlevels = icontent_get_noteparentinglevels($pagenote->path);
 
 			// Div list page notes
---
+++ removenote.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Delete icontent page
+ *
+ * @package    mod_icontent
+ * @copyright  2004-2011 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require(dirname(__FILE__).'/../../config.php');
+require_once(dirname(__FILE__).'/locallib.php');
+
+$id			= required_param('id', PARAM_INT);      // Course Module ID
+$pnid		= required_param('pnid', PARAM_INT); 	// page note ID
+$confirm	= optional_param('confirm', 0, PARAM_BOOL);
+
+$cm = get_coursemodule_from_id('icontent', $id, 0, false, MUST_EXIST);
+$course = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);
+$icontent = $DB->get_record('icontent', array('id'=>$cm->instance), '*', MUST_EXIST);
+
+require_login($course, false, $cm);
+require_sesskey();
+
+$context = context_module::instance($cm->id);
+require_capability('mod/icontent:edit', $context);
+
+$PAGE->set_url('/mod/icontent/delete.php', array('id'=>$id, 'pageid'=>$pageid));
+
+$page = $DB->get_record('icontent_pages', array('id'=>$pageid, 'icontentid'=>$icontent->id), '*', MUST_EXIST);
+
+
+// Header and strings.
+$PAGE->set_title($icontent->name);
+$PAGE->set_heading($course->fullname);
+
+// Form processing.
+if ($confirm) {  // the operation was confirmed.
+    $fs = get_file_storage();
+ 	// deletando todos os arquivos e registros vinculados s pagina
+    $fs->delete_area_files($context->id, 'mod_icontent', 'page', $page->id);
+	$fs->delete_area_files($context->id, 'mod_icontent', 'bgpage', $page->id);
+	
+	$DB->delete_records('icontent_pages_displayed', array('pageid'=>$page->id));
+	$DB->delete_records('icontent_pages_questions', array('pageid'=>$page->id));
+	icontent_remove_notes($page->id); // remove notes and notes like
+    $DB->delete_records('icontent_pages', array('id'=>$page->id));
+
+    icontent_preload_pages($icontent); // Fix structure.
+
+    redirect('view.php?id='.$cm->id);
+}
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading($icontent->name);
+
+// The operation has not been confirmed yet so ask the user to do so.
+$strconfirm = get_string('confpagedelete', 'mod_icontent');
+
+echo '<br />';
+$continue = new moodle_url('/mod/icontent/delete.php', array('id'=>$cm->id, 'pageid'=>$page->id, 'confirm'=>1));
+$cancel = new moodle_url('/mod/icontent/view.php', array('id'=>$cm->id, 'pageid'=>$page->id));
+echo $OUTPUT->confirm("<strong>$page->title</strong><p>$strconfirm</p>", $continue, $cancel);
+
+echo $OUTPUT->footer();

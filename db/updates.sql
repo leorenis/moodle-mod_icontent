c7824e22de9fc9b7141a469a9ea5349082f8c79d
Léo Renis Santos <leorenis@gmail.com>
2016-07-08 20:16:23 -0300

    Update SQL
    Update SQL

--- db/updates.sql
+++ db/updates.sql
@@ -1,11 +1,11 @@
-﻿-- Updates 2016070800
+﻿-- Updates 2016070800 - OK
 ALTER TABLE `moodle28`.`mdl_icontent_pages` 
 DROP COLUMN `imagetransitiontype`,
 DROP COLUMN `texttransitiontype`,
 DROP COLUMN `prevtransitiontype`,
 CHANGE COLUMN `nexttransitiontype` `transitioneffect` VARCHAR(255) NULL DEFAULT '0';
 
--- Updates 2016062900
+-- Updates 2016062900 - OK
 ALTER TABLE `moodle28`.`mdl_icontent` DROP COLUMN `maxquestionsperpages`;
 
 -- Updates 2016060700 - OK

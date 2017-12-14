2ffd1dbea6c4d253ea2076b4c55dbd38463caba5
Kiril <kiril@devlion.co>
2017-12-14 10:14:02 +0200

    Update Query LIMIT 1

--- locallib.php
+++ locallib.php
@@ -1026,7 +1026,7 @@ function icontent_get_attempt_summary_by_page($pageid, $cmid){
     WHERE  pq.pageid = ?
     AND pq.cmid = ?
     AND qa.userid = ?
-    GROUP BY qa.timecreated;";
+    GROUP BY qa.timecreated limit 1;";
     // Get record
     $attemptsummary = $DB->get_record_sql($sql, array($pageid, $cmid, $USER->id));
     // Checks if a property isn't empty.

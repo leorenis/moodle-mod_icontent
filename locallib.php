e1c1fd2db82223d3cf45fd04c0a5d2c1a9a6380f
ilya dymshits <ilya@devlion.co>
2018-01-17 18:27:04 +0300

    add #tab_page-number

--- locallib.php
+++ locallib.php
@@ -106,7 +106,7 @@ function icontent_get_toc($pages, $page, $icontent, $cm, $edit) {
             $i ++;
             $title = trim(format_string($pg->title, true, array('context'=>$context)));
             $toc .= html_writer::start_tag('li', array('class' => 'load-page page'.$pg->pagenum.' '.$page_title_indent.' '.$visited_page, 'title' => s($title), 'data-pagenum' => $pg->pagenum, 'data-cmid' => $pg->cmid, 'data-sesskey' => sesskey(), 'data-totalpages' => $tpages)); // Inicio <li>
-            $toc .= html_writer::link('#', $title);
+            $toc .= html_writer::link('#tab'.$pg->pagenum, $title);
             // Actions
             $toc .= html_writer::start_tag('div', array('class' => 'action-list')); // Inicio <div>
             if ($i != 1) {
@@ -144,7 +144,7 @@ function icontent_get_toc($pages, $page, $icontent, $cm, $edit) {
             if(!$pg->hidden){
                 $title = trim(format_string($pg->title, true, array('context'=>$context)));
                 $toc .= html_writer::start_tag('li', array('class' =>'load-page page'.$pg->pagenum.' '.$page_title_indent.' '.$visited_page, 'data-pagenum' => $pg->pagenum, 'data-cmid' => $pg->cmid, 'data-sesskey' => sesskey(), 'data-totalpages' => $tpages));
-                $toc .= html_writer::link('#', $title );
+                $toc .= html_writer::link('#tab'.$pg->pagenum, $title );
                 $toc .= html_writer::end_tag('li');
             }
         }

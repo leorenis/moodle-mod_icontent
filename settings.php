4d79f33df740fab523c43d6e6a74a25da8d2ef36
AL Rachels <drachels@drachels.com>
2024-02-12 16:21:08 -0600

    Update settings.php

--- settings.php
+++ settings.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Administration settings definitions for the iContent module.
+ * Administration default settings definitions for the iContent module.
  *
  * @package    mod_icontent
  * @copyright  2023 AL Rachels (drachels@drachels.com)
@@ -26,13 +26,14 @@ defined('MOODLE_INTERNAL') || die;
 use mod_icontent\local\keyboards;
 
 if ($ADMIN->fulltree) {
-    // Changed to this newer format 03/10/2019.
+    // Changed to this format 03/10/2019.
     require_once(__DIR__ . '/lib.php');
+    require_once(dirname(__FILE__).'/locallib.php');
 
     // Availability settings.
     $settings->add(new admin_setting_heading('mod_icontent/availibility', get_string('availability'), ''));
 
-    // Recent activity setting.
+    // 20231230 Recent activity setting.
     $name = new lang_string('showrecentactivity', 'icontent');
     $description = new lang_string('showrecentactivityconfig', 'icontent');
     $settings->add(new admin_setting_configcheckbox('mod_icontent/showrecentactivity',
@@ -46,77 +47,89 @@ if ($ADMIN->fulltree) {
     // Need and appearance setting for background color, border color, border width max number of
     // pages, show notes area, max number of notes per page and the progress bar.
 
-    // Date format setting.
+    // 20240205Slide background colour setting.
+    $name = 'mod_icontent/bgcolor';
+    $title = get_string('bgcolor', 'icontent');
+    $description = get_string('bgcolorhelp_help', 'icontent');
+    $default = get_string('bgcolor_color', 'icontent');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // 20240205 Slide border colour setting.
+    $name = 'mod_icontent/bordercolor';
+    $title = get_string('bordercolor', 'icontent');
+    $description = get_string('bordercolorhelp_help', 'icontent');
+    $default = get_string('bordercolor_color', 'icontent');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // 20240205 iContent border width setting.
+    $name = 'mod_icontent/borderwidth';
+    $title = get_string('borderwidth', 'icontent');
+    $description = get_string('borderwidthhelp_help', 'icontent');
+    $default = 0;
+    $options = icontent_add_borderwidth_options();
+    $settings->add(new admin_setting_configselect($name, $title, $description, $default, $options, 1));
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // 20240205 iContent Maximum number of pages.
+    $maxpages = [];
+    for ($i = 0; $i <= 56; $i++) {
+        $maxpages[] = $i;
+    }
+    $name = 'mod_icontent/maxpages';
+    $title = get_string('maxpages', 'icontent');
+    $description = get_string('maxpageshelp_help', 'icontent');
+    $default = 35;
+    $settings->add(new admin_setting_configselect($name, $title, $description, $default, $maxpages));
+
+
+    // 20210708 iContent Show notes area.
+    $name = 'mod_icontent/shownotesarea';
+    $title = get_string('shownotesarea', 'icontent');
+    $description = get_string('shownotesarea_help', 'icontent');
+    $default = 1;
+    $settings->add(new admin_setting_configselect($name, $title, $description, $default,
+        [
+            '0' => get_string('no'),
+            '1' => get_string('yes'),
+        ]
+    ));
+
+    // 20240205 iContent Maximum number of notes per page.
+    $maxnotesperpages = [];
+    for ($i = 0; $i <= 50; $i++) {
+        $maxnotesperpages[] = $i;
+    }
+    $name = 'mod_icontent/maxnotesperpages';
+    $title = get_string('maxnotesperpages', 'icontent');
+    $description = get_string('maxnotesperpageshelp_help', 'icontent');
+    $default = 50;
+    $settings->add(new admin_setting_configselect($name, $title, $description, $default, $maxnotesperpages));
+
+
+    // 20240205 iContent Show progress bar.
+    $name = 'mod_icontent/progressbar';
+    $title = get_string('progressbar', 'icontent');
+    $description = get_string('progressbar_help', 'icontent');
+    $default = 1;
+    $settings->add(new admin_setting_configselect($name, $title, $description, $default,
+        [
+            '0' => get_string('no'),
+            '1' => get_string('yes'),
+        ]
+    ));
+
+    // 20231230 Date format setting.
     $settings->add(new admin_setting_configtext(
         'mod_icontent/dateformat',
         get_string('dateformat', 'icontent'),
         get_string('dateformatconfig', 'icontent'),
-        'M d, Y G:i A', PARAM_TEXT, 15)
+        'M d, Y G:i A',
+        PARAM_TEXT,
+        25)
     );
-
-    // I have temporarily left these as examples from MooTyper to draw from.
-    /*
-    // Statistics bar colour setting.
-    $name = 'mod_icontent/statscolor';
-    $title = get_string('statscolor_title', 'icontent');
-    $description = get_string('statscolor_descr', 'icontent');
-    $default = get_string('statscolor_colour', 'icontent');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $settings->add($setting);
-
-    // Key top text colour setting.
-    $name = 'mod_icontent/normalkeytoptextc';
-    $title = get_string('normalkeytoptextc_title', 'icontent');
-    $description = get_string('normalkeytoptextc_descr', 'icontent');
-    $default = get_string('normalkeytoptextc_colour', 'icontent');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $settings->add($setting);
-
-    // Key top colour setting.
-    $name = 'mod_icontent/normalkeytops';
-    $title = get_string('normalkeytops_title', 'icontent');
-    $description = get_string('normalkeytops_descr', 'icontent');
-    $default = get_string('normalkeytops_colour', 'icontent');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $settings->add($setting);
-
-    // Keyboard background colour setting.
-    $name = 'mod_icontent/keyboardbgc';
-    $title = get_string('keyboardbgc_title', 'icontent');
-    $description = get_string('keyboardbgc_descr', 'icontent');
-    $default = get_string('keyboardbgc_colour', 'icontent');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $settings->add($setting);
-
-    // Cursor colour setting.
-    $name = 'mod_icontent/cursorcolor';
-    $title = get_string('cursorcolor_title', 'icontent');
-    $description = get_string('cursorcolor_descr', 'icontent');
-    $default = get_string('cursorcolor_colour', 'icontent');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $settings->add($setting);
-
-    // Text background colour setting.
-    $name = 'mod_icontent/textbgc';
-    $title = get_string('textbgc_title', 'icontent');
-    $description = get_string('textbgc_descr', 'icontent');
-    $default = get_string('textbgc_colour', 'icontent');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $settings->add($setting);
-
-    // Text error background colour setting.
-    $name = 'mod_icontent/texterrorcolor';
-    $title = get_string('texterrorcolor_title', 'icontent');
-    $description = get_string('texterrorcolor_descr', 'icontent');
-    $default = get_string('texterrorcolor_colour', 'icontent');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $settings->add($setting);
-    */
 }

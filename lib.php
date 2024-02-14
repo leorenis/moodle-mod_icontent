f8303308cc95eec0802d52d0f40558e2d6524f58
AL Rachels <drachels@drachels.com>
2024-02-14 13:18:01 -0600

    Update lib.php

--- lib.php
+++ lib.php
@@ -491,7 +491,7 @@ function icontent_pluginfile($course, $cm, $context, $filearea, $args, $forcedow
     $fullpath = "/$context->id/mod_icontent/$filearea/$itemid/$relativepath";
 
     $fs = get_file_storage();
-    if (!$file = $fs->get_file_by_hash(sha1($fullpath)) || $file->is_directory()) {
+    if ((!$file = $fs->get_file_by_hash(sha1($fullpath))) || ($file->is_directory())) {
         return false;
     }
 

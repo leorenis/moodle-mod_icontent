b51ef9e4654a24f077360ae7fdba52d95f2821a3
AL Rachels <drachels@drachels.com>
2024-07-27 17:56:16 -0500

    Update view.php

--- view.php
+++ view.php
@@ -105,7 +105,8 @@ if ($icontent->intro) {
     echo $OUTPUT->box(format_module_intro('icontent', $icontent, $cm->id), 'generalbox mod_introbox', 'icontentintro');
 }
 // Content box.
-if ($pages > 5) {
+//echo count($pages);
+if (count($pages) > 5) {
     echo icontent_simple_paging_button_bar($pages, $cm->id, $startwithpage);
 } else {
     echo icontent_full_paging_button_bar($pages, $cm->id, $startwithpage);
